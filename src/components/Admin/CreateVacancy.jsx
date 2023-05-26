b5de93177991a9f7d8ad6266297aebc0ab4436af
Carlos Arturo Miranda Trujano <arturo.miranda@CDMX.PTH>
2023-05-26 11:32:14 -0600

    checkbox and date in form

--- src/components/Admin/CreateVacancy.jsx
+++ src/components/Admin/CreateVacancy.jsx
@@ -1,42 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { getDatabase, ref, push } from "firebase/database";
+import { InputDate, InputForm, LabelForm } from "../Form/InputForm";
 
-function CreateVacancy(props) {
-    // const {functionsForm} = props
-    // const {
-    //     saveData
-    // } = functionsForm
+function CreateVacancy() {
+	const [vacancyData, setVacancyData] = useState({});
 
+	const changeHandler = (event) => {
+		const property = event.target.name;
+		const value = event.target.value;
+		setVacancyData({ ...vacancyData, [property]: value });
+	};
+
+	const database = getDatabase();
+	const vacancyRef = ref(database, "/vacancy");
+
+	const saveData = () => {
+		return push(vacancyRef, vacancyData);
+		// console.log(vacancyData);
+		// UPDATE DATA
+		// push(child(ref(database), "/vacancy"));
+	};
+
+	// ----- T O  E D I T  F O R M
+	// useEffect(() => {
+	// 	onValue(vacancyRef, (snapshot) => {
+	// 		console.log(snapshot.val());
+	// 	});
+	// }, []);
+
+	// function date() {
+	// 	return (
+	// 		`${current.getFullYear()}-${
+	// 			current.getMonth() + 1
+	// 		}-${current.getDate()}`
+	// 	)
+	// }
+	const current = new Date();
+	const date = `${current.getFullYear()}-${
+		current.getMonth() + 1
+	}-${current.getDate()}`;
+	// const dateToday = new Date(date);
+	const changeDate = (event) => {
+		const property = event.target.name;
+		const value = date;
+		setVacancyData({ ...vacancyData, [property]: value });
+	};
 	return (
 		<section className="flex justify-center flex-col items-center pb-28 px-2">
 			<form className="w-full max-w-4xl">
-				<div className="my-4">
-					<label htmlFor="" className="text-gray-300">
-						Vacante:
-					</label>
-					<input
-						type="text"
-						name="vacancyTitle"
-						className="block w-full rounded-md border-0 focus:outline-none focus:ring-1 focus:ring-gray-100 py-1 px-1.5 text-gray-100 bg-[#ffffff3b]"
-						// onChange={changeHandler}
-					/>
+				<div className="flex justify-end">
+					<InputDate on={changeDate} />
+				</div>
+				<InputForm label="Vacante" name="vacancyTitle" on={changeHandler} />
+				<InputForm label="Área" name="areaVacancy" on={changeHandler} />
+				<InputForm
+					label="Categoría"
+					name="categoryVacancy"
+					on={changeHandler}
+				/>
+				<InputForm
+					label="Subcategoría"
+					name="subcategoryVacancy"
+					on={changeHandler}
+				/>
+				<InputForm label="Salario" name="salaryVacancy" on={changeHandler} />
+				<InputForm
+					label="Jornada Laboral"
+					name="typeVacancy"
+					on={changeHandler}
+				/>
+				<InputForm label="Modalidad" name="typeLocation" on={changeHandler} />
+				<InputForm label="Ubicación" name="locationPlace" on={changeHandler} />
+				<InputForm
+					label="Educación mínima requerida"
+					name="requiredVacancy"
+					on={changeHandler}
+				/>
+				<LabelForm label="Descripción" name="content" on={changeHandler} />
+
+				<div>
+					<button
+						className="bg-red-600 py-1 px-4 rounded-md font-semibold text-gray-100 hover:bg-red-700"
+						type="button"
+						onClick={saveData}
+					>
+						Publicar
+					</button>
 				</div>
 			</form>
-			<div className="flex justify-end w-full max-w-4xl">
-				{/* <button
-					// prop1={ props.children }
-					className="border border-gray-400 rounded-md px-4 py-1 text-gray-400 hover:text-gray-200 hover:border-gray-200"
-				>
-					Logout
-				</button> */}
-                <button
-					onClick={ props.children }
-					className="border border-gray-400 rounded-md px-4 py-1 text-gray-400 hover:text-gray-200 hover:border-gray-200"
-				>
-					LogIn
-				</button>
-
-                {/* {props.children} */}
-			</div>
 		</section>
 	);
 }
--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -1,47 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Template } from "../../templates/Template";
-import {
-	getDatabase,
-	ref,
-	onValue,
-	child,
-	push,
-	update,
-	set,
-} from "firebase/database";
 import { onAuthStateChanged } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { InputForm, LabelForm } from "../Form/InputForm";
 import { Error404 } from "../../views/404";
+import { CreateVacancy } from "./CreateVacancy";
 
 function Admin() {
-	const [vacancyData, setVacancyData] = useState({});
 	const [test, setTest] = useState(false);
-
-	const changeHandler = (event) => {
-		const property = event.target.name;
-		const value = event.target.value;
-		setVacancyData({ ...vacancyData, [property]: value });
-	};
-
-	const database = getDatabase();
-	const vacancyRef = ref(database, "/vacancy");
-
-	// useEffect(() => {
-	// 	onValue(vacancyRef, (snapshot) => {
-	// 		console.log(snapshot.val());
-	// 	});
-	// }, []);
-
-	const saveData = () => {
-		return push(vacancyRef, vacancyData);
-		// console.log(vacancyData);
-		// UPDATE DATA
-		// push(child(ref(database), "/vacancy"));
-	};
-
 	const auth = getAuth();
 	const navigate = useNavigate();
 
@@ -88,74 +55,18 @@ function Admin() {
 	return (
 		<Template>
 			{test ? (
-				<section className="flex justify-center flex-col items-center pb-28 px-2">
-					<form className="w-full max-w-4xl">
-						<InputForm
-							label="Fecha de Publicación"
-							name="dateVacancy"
-							on={changeHandler}
-						/>
-						<InputForm label="Vacante" name="vacancyTitle" on={changeHandler} />
-						<InputForm label="Área" name="areaVacancy" on={changeHandler} />
-						<InputForm
-							label="Categoría"
-							name="categoryVacancy"
-							on={changeHandler}
-						/>
-						<InputForm
-							label="Subcategoría"
-							name="subcategoryVacancy"
-							on={changeHandler}
-						/>
-						<InputForm
-							label="Salario"
-							name="salaryVacancy"
-							on={changeHandler}
-						/>
-						<InputForm
-							label="Jornada Laboral"
-							name="typeVacancy"
-							on={changeHandler}
-						/>
-						<InputForm
-							label="Modalidad"
-							name="typeLocation"
-							on={changeHandler}
-						/>
-						<InputForm
-							label="Ubicación"
-							name="locationPlace"
-							on={changeHandler}
-						/>
-						<InputForm
-							label="Educación mínima requerida"
-							name="requiredVacancy"
-							on={changeHandler}
-						/>
-						<LabelForm label="Descripción" name="content" on={changeHandler} />
-						<div>
-							<button
-								className="bg-red-600 py-1 px-4 rounded-md font-semibold text-gray-100 hover:bg-red-700"
-								type="button"
-								onClick={saveData}
-							>
-								Publicar
-							</button>
-						</div>
-					</form>
-					<div className="flex justify-end w-full max-w-4xl">
-						<button
-							onClick={handleLogout}
-							className="border border-gray-400 rounded-md px-4 py-1 text-gray-400 hover:text-gray-200 hover:border-gray-200"
-						>
-							Logout
-						</button>
-					</div>
-					{/* (<CreateVacancy>{ function active() {console.log('666')} }</CreateVacancy>)) */}
-				</section>
+				<CreateVacancy />
 			) : (
 				<Error404 />
 			)}
+			<div className="flex justify-end w-full max-w-4xl">
+				<button
+					onClick={handleLogout}
+					className="border border-gray-400 rounded-md px-4 py-1 text-gray-400 hover:text-gray-200 hover:border-gray-200"
+				>
+					Logout
+				</button>
+			</div>
 		</Template>
 	);
 }
